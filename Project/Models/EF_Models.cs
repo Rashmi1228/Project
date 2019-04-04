80b25a7db7df28c3d276a33f6e0fde48eb6f4764
Rushikesh Maheshwari <rushikesh.maheshwari12@outlook.com>
2019-04-04 02:20:03 -0400

    rushi - API classes added

--- Project/Models/EF_Models.cs
+++ Project/Models/EF_Models.cs
@@ -8,7 +8,44 @@ namespace Project.Models
 {
     public class EF_Models
     {
+        //API 1
         public class C1
+        {
+            [Key]
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+            public string Date { get; set; }
+        }
+
+        //API 2
+        public class C2
+        {
+            [Key]
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+            public string Date { get; set; }
+        }
+
+        //API 3
+        public class C3
+        {
+            [Key]
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+            public string Date { get; set; }
+        }
+
+        //API 4
+        public class C4
+        {
+            [Key]
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+            public string Date { get; set; }
+        }
+
+        //API 5
+        public class C5
         {
             [Key]
             public string Symbol { get; set; }
